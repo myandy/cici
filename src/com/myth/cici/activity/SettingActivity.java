394918d31e6c059b602e0c0107988cf82007a685
andymao <andymao@pptv.com>
2015-04-14 16:27:34 +0800

    豌豆荚测试

--- src/com/myth/cici/activity/SettingActivity.java
+++ src/com/myth/cici/activity/SettingActivity.java
@@ -3,7 +3,9 @@ package com.myth.cici.activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -12,10 +14,16 @@ import com.myth.cici.BaseActivity;
 import com.myth.cici.MyApplication;
 import com.myth.cici.R;
 import com.myth.cici.db.YunDatabaseHelper;
-import com.qq.e.appwall.GdtAppwall;
+import com.wandoujia.ads.sdk.Ads;
 
 public class SettingActivity extends BaseActivity
 {
+
+    private static final String APP_ID = "100025361";
+
+    private static final String SECRET_KEY = "0366690459f6380608431eef19bf04f8";
+
+    private static final String APP_WALL = "799ffe69dd79b23ba44a4642378a22a6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,12 +31,43 @@ public class SettingActivity extends BaseActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         initView();
+
+        new AsyncTask<Void, Void, Boolean>()
+        {
+            @Override
+            protected Boolean doInBackground(Void... params)
+            {
+                try
+                {
+                    Ads.init(mActivity, APP_ID, SECRET_KEY);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Log.e("ads-sample", "error", e);
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean success)
+            {
+
+                if (success)
+                {
+                    /**
+                     * pre load
+                     */
+                    Ads.preLoad(APP_WALL, Ads.AdFormat.appwall);
+
+                }
+            }
+        }.execute();
+
     }
 
     private void initView()
     {
-
-        final GdtAppwall appwall = new GdtAppwall(this, "1104396282", "2090000295819515", false);
 
         refreshYun();
         refreshTypeface();
@@ -93,15 +132,15 @@ public class SettingActivity extends BaseActivity
                 String[] s = {mActivity.getString(R.string.check_true), mActivity.getString(R.string.check_false)};
                 new AlertDialog.Builder(mActivity).setSingleChoiceItems(s,
                         MyApplication.getCheckAble(mActivity) ? 0 : 1, new DialogInterface.OnClickListener()
-                {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
-                        MyApplication.setCheckAble(mActivity, which == 0);
-                        refreshCheck();
-                        dialog.dismiss();
-                    }
-                }).show();
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                MyApplication.setCheckAble(mActivity, which == 0);
+                                refreshCheck();
+                                dialog.dismiss();
+                            }
+                        }).show();
             }
         });
         findViewById(R.id.item_about).setOnClickListener(new OnClickListener()
@@ -113,15 +152,15 @@ public class SettingActivity extends BaseActivity
                 startActivity(new Intent(mActivity, AboutActivity.class));
             }
         });
-        // findViewById(R.id.item_ad).setOnClickListener(new OnClickListener()
-        // {
-        //
-        // @Override
-        // public void onClick(View v)
-        // {
-        // appwall.doShowAppWall();
-        // }
-        // });
+        findViewById(R.id.item_ad).setOnClickListener(new OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                Ads.showAppWall(mActivity, APP_WALL);
+            }
+        });
     }
 
     private void refreshYun()
